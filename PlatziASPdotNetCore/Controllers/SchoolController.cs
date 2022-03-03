193a2cbed8aa8f2210e34e3167547b76e2ef3a57
Vicente Estrada <67867277+vicente100est@users.noreply.github.com>
2022-03-03 17:31:27 -0600

    first commit

---
+++ PlatziASPdotNetCore/Controllers/SchoolController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PlatziASPdotNetCore.Controllers
+{
+    public class SchoolController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
